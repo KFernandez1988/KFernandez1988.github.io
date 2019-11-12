16d9410cf825c70e8082aabf68e133e5bd5aae4e
kevin fernandez <kevinfernandezsoftdev@gmail.com>
2019-11-12 12:16:17 -0400

    Fixing a lot of simantic problems

--- js/content.js
+++ js/content.js
@@ -2,3 +2,81 @@
  * Use AJAX to load the JSON and manipulate the HTML
  * https://joshbloom.github.io/dws1/data/hikersguide.json
 */
+
+   function getInformation(){
+    fetch("https://joshbloom.github.io/dws1/data/hikersguide.json")
+    .then((info) => data = info.json()).then(
+       data => {
+          console.log(data.locations);
+
+          const locations = document.querySelector('.locations')
+          let location = data.locations[0];
+          locations.innerHTML = `<h2>${location.city}, ${location.state}</h2>
+          <p><em><b>${location.title}</b></em></p>
+          <p>${location.text}</p>`;
+
+         
+
+          const posts = document.querySelector('.posts')
+          let postsList = '';
+
+          data.posts.forEach( post => {
+          
+            postsList += `<li>
+                           <article>
+                              <img src="${post.imageURL}" width="400" alt="">
+                              <h3>${post.title} </h3>
+                              <p>${post.text}</p>
+                              <button>Read more</button>
+                           </article>
+                          </li>`;
+          });
+
+
+          posts.insertAdjacentHTML('afterbegin', postsList);
+
+          const events = document.querySelector('.events')
+          let eventList = '';
+
+          data.events.forEach( ev => {
+          
+            eventList += `<li>
+                        <div>
+                           <h5>${ev.date}</h5>
+                           <h4><em>${ev.title}</em></h4>
+                           <p>${ev.text}</p>
+                        </div>
+                         </li>`;
+          });
+
+          events.insertAdjacentHTML('afterbegin', eventList);
+
+          const hikers = document.querySelector('.hikers-list');
+          let hikersList = '';
+          data.hikers.forEach(el => {
+             
+            hikersList += `<li>
+            <div>
+                <img src="${el.imageURL}" alt="">
+                <p><strong>${el.lastname},${el.firstname}</strong></p>
+                <p>Orlando, FL</p>
+            </div>
+        </li>`;
+
+          });
+
+          hikers.insertAdjacentHTML('afterbegin', hikersList);
+
+
+
+          const about = document.querySelector('.about');
+
+          about.innerHTML = `<p><em> ${data.about.title} </em></p>
+                             <p> ${data.about.text}</p>`;
+     
+       }
+    );
+}
+
+
+document.addEventListener('load', getInformation());
